ac576b0fa4084c159836cfed98879a07ab2fb71f
vitas-pm <pm@vitas.ai>
2022-03-17 16:55:00 +0100

    fixes

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory
 from flask.logging import create_logger
 import logging
 
