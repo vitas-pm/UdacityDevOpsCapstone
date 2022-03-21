6ed6577c47ca0c3d01805c52398d1cbc4868e9b5
vitas-pm <pm@vitas.ai>
2022-03-21 01:46:39 +0100

    proper circleci

--- increase_version.sh
+++ increase_version.sh
@@ -1,7 +1,8 @@
 current_version=`cat version.txt`     #v1.xx
 echo "current version: $current_version"
 to_int=${current_version:3}
-((to_int=to_int+1))
+echo "to_int: $to_int"
+to_int=$((to_int+1))
 new_version="v1.${to_int}"
 echo "next version: $new_version"
 echo $new_version > version.txt
